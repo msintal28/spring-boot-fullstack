2d026cef32adb79386533c02f98623fdc383aa9a
msintal <mathias.sintal@gmail.com>
2023-12-03 23:08:48 +0100

    test workflow

--- backend/src/main/java/com/amigoscode/SpringBootExampleApplication.java
+++ backend/src/main/java/com/amigoscode/SpringBootExampleApplication.java
@@ -1,18 +1,16 @@
 package com.amigoscode;
 
-import com.amigoscode.customer.Customer;
 import com.amigoscode.customer.CustomerRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
-import java.util.List;
-
 @SpringBootApplication
 public class SpringBootExampleApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringBootExampleApplication.class, args);
     }
 
