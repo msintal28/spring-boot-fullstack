e8b99e2ce264f819aa0093470366351e8076ad3b
msintal <mathias.sintal@gmail.com>
2023-12-10 16:29:38 +0100

    CORS, Axios, fetch customers card components and sidebar

--- frontend/react/src/App.jsx
+++ frontend/react/src/App.jsx
@@ -1,66 +1,65 @@
-import UserProfile from "./UserProfile.jsx";
-import {useState, useEffect} from "react";
+import {Spinner, Text, Wrap, WrapItem} from "@chakra-ui/react";
+import SidebarWithHeader from "./components/shared/SideBar.jsx";
+import {useEffect, useState} from "react";
+import {getCustomers} from "./services/client.js";
+import CardWithImage from "./components/Card.jsx";
 
-const users = [
-    {
-        name: "Jamila",
-        age: 22,
-        gender: "FEMALE"
-    }, {
-        name: "Marco",
-        age: 24,
-        gender: "MALE"
-    }
-    , {
-        name: "Flaminco",
-        age: 27,
-        gender: "MALE"
-    }
-]
-
-const UserProfiles = ({users}) => (
-    <div>
-        {users.map((user, index) => (
-            <UserProfile
-                key={index}
-                name={user.name}
-                age={user.age}
-                gender={user.gender}
-                imageNumber={index}
-            />
-        ))}
-    </div>
-)
 
 function App() {
 
-    const [counter, setCounter] = useState(0);
-    const [isLoading, setIsLoading] = useState(false)
-    console.log("before useeffect")
+    const [customers, setCustomers] = useState([]);
+    const [loading, setLoading] = useState(false);
+
     useEffect(() => {
-        console.log("in useEffect")
-        setIsLoading(true)
-        setTimeout(() => {
-            setIsLoading(false)
-            console.log("inside timeout")
-        }, 4000)
-        return () => {
-            console.log("cleanup function")
-        }
+        setLoading(true);
+        getCustomers().then(res => {
+            setCustomers(res.data)
+            console.log(res.data)
+        }).catch(err => {
+            console.log(err)
+        }).finally(() => {
+            setLoading(false);
+        })
     }, [])
-    console.log("after useeffect")
-    if (isLoading) {
-        return "loading..."
+
+    if (loading) {
+        return (
+            <SidebarWithHeader>
+                <Spinner
+                    thickness='4px'
+                    speed='0.65s'
+                    emptyColor='gray.200'
+                    color='blue.500'
+                    size='xl'
+                />
+            </SidebarWithHeader>
+        )
+    }
+
+    if (customers.length <= 0) {
+        return (
+            <SidebarWithHeader>
+                <Text>No customers available</Text>
+            </SidebarWithHeader>
+        )
     }
     return (
-        <div>
-            <button onClick={() => {
-                setCounter(prevState => prevState + 1)
-            }}>Increment counter</button>
-            <h1>{counter}</h1>
-            <UserProfiles users={users}/>
-        </div>
-    );
+        <SidebarWithHeader>
+            <Wrap justify={"center"} spacing={"10px"}>
+                {customers.map((customer, index) => (
+                    <WrapItem key={index}>
+                        <CardWithImage
+                            {...customer}
+                            gender={"women"}
+                            imageNumber={index}
+                        />
+                    </WrapItem>
+
+                ))}
+            </Wrap>
+        </SidebarWithHeader>
+    )
+
 }
 
 export default App
